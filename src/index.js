1bc2b528283e2c887acbdcb678d5fd816249ae68
Patrick Maloney <37421561+PatrickBMaloney@users.noreply.github.com>
2020-12-25 16:50:17 -0500

    Using inserting JavaScript in JSX

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fName = "Patrick";
+const lName = "Maloney";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
 ReactDOM.render(
   <div>
-    <h1>Hellow World!</h1>
-    <p>This is a paragraph</p>
+    <h1>Hello {fName + " " + lName}!</h1>
+    <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
+    <ul>
+      <li>Bacon</li>
+      <li>Mushrroms</li>
+      <li>Jamon</li>
+    </ul>
+    <p>Copyright {year}</p>
   </div>,
   document.getElementById("root")
 );
