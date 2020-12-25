7b7e6283bfbd341acc06e056b9ed3df69f5125f1
Patrick Maloney <37421561+PatrickBMaloney@users.noreply.github.com>
2020-12-25 18:55:40 -0500

    In-line styles

--- src/index.js
+++ src/index.js
@@ -5,11 +5,32 @@ const fName = "Patrick";
 const lName = "Maloney";
 const currentDate = new Date();
 const year = currentDate.getFullYear();
+const currentTime = currentDate.getHours();
 const img = "https://picsum.photos/200";
+
+const customStyle = {
+  color: "",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+let greeting;
+if (currentTime < 12) {
+  customStyle.color = "red";
+  greeting = "Good Morning";
+} else if (currentTime < 18) {
+  customStyle.color = "green";
+  greeting = "Good Afternoon";
+} else {
+  customStyle.color = "blue";
+  greeting = "Good Evening";
+}
 
 ReactDOM.render(
   <div>
-    <h1>Hello {fName + " " + lName}!</h1>
+    <h1 style={customStyle}>
+      {greeting} {fName + " " + lName}!
+    </h1>
     <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
     <ul>
       <li>Bacon</li>
@@ -19,7 +40,7 @@ ReactDOM.render(
     <img className="food-img" alt="random" src={img} />
     <img
       className="food-img"
-      alt="random"
+      alt="dog"
       src="https://i.insider.com/5484d9d1eab8ea3017b17e29?width=600&format=jpeg&auto=webp"
     />
 
