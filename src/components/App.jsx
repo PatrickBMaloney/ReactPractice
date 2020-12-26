9e6af3c4b652db1b931770f97637478bb0f2f9c4
Patrick Maloney <37421561+PatrickBMaloney@users.noreply.github.com>
2020-12-26 16:15:15 -0500

    Add contacts component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Heading from "./Heading";
 import List from "./List";
+import Card from "./Card";
 
 const fName = "Patrick";
 const lName = "Maloney";
@@ -43,6 +44,26 @@ function App() {
         className="food-img"
         alt="dog"
         src="https://i.insider.com/5484d9d1eab8ea3017b17e29?width=600&format=jpeg&auto=webp"
+      />
+
+      <h1>My Contacts</h1>
+      <Card
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        phone="+123 456 789"
+        email="b@beyonce.com"
+      />
+      <Card
+        name="Jack Bauer"
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        phone="+987 654 321"
+        email="jack@nowhere.com"
+      />
+      <Card
+        name="Chuck Norris"
+        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        phone="+918 372 574"
+        email="gmail@chucknorris.com"
       />
 
       <p>Copyright {year}</p>
